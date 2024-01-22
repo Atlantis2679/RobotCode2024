d8fe69f9651ca43384e74cd3e580786d10291bbc
Ruth Bilu <ruti.bilu@gmail.com>
2024-01-22 21:11:56 +0200

    adding elevator

---
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.elevator.io.ElevatorIO;
+import frc.robot.subsystems.elevator.io.ElevatorIOSparkMax;
+
+public class Elevator extends SubsystemBase{
+    private final ElevatorIO io = new ElevatorIOSparkMax(null);
+
+    public void setSpeed(double speed){
+        speed = MathUtil.clamp(speed, 0, 0);
+        io.setSpeed(speed);
+    }
+
+    protected double getEncoder(){
+        return ElevatorIO.getEncoder(encoderValue);
+    }
+}
--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -4,8 +4,12 @@ import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class ElevatorConstants {
-    public final static CANSparkMax ELEVATOR_MOTOR_RIGHT = 0 ;
-    public final static CANSparkMax ELEVATOR_MOTOR_LETF = 0 ;
-     public final static DutyCycleEncoder ELEVATOR_ENCODER = 0 ;
+    public final static int ELEVATOR_MOTOR_RIGHT_ID = 0 ;
+    public final static int ELEVATOR_MOTOR_LETF_ID = 0 ;
+    public final static double ELEVATOR_ENCODER = 0 ;
+
+    public final static double KP = 0;
+    public final static double KI = 0;
+    public final static double KD = 0;
     
 }
