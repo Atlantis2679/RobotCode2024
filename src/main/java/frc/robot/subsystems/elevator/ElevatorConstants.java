a728b31b4a0f2715fe4b06c402723d8e144a5d17
Roi Bar <roi3bar@gmail.com>
2024-01-22 15:46:43 +0200

    elavator things

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,11 +1,13 @@
 package frc.robot.subsystems.elevator;
 
-import com.revrobotics.CANSparkMax;
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class ElevatorConstants {
-    public final static CANSparkMax ELEVATOR_MOTOR_RIGHT = 0 ;
-    public final static CANSparkMax ELEVATOR_MOTOR_LETF = 0 ;
-     public final static DutyCycleEncoder ELEVATOR_ENCODER = 0 ;
+    public final static int ELEVATOR_MOTOR_RIGHT = 0 ;
+    public final static int ELEVATOR_MOTOR_LETF = 0 ;
+    public final static int ELEVATOR_ENCODER = 0 ;
+
+    public final static double KP = 0;
+    public final static double KI = 0;
+    public final static double KD = 0;
     
 }
