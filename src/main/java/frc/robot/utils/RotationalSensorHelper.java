6201a64440f21b797b955d4f7c757701fea1bd80
Roi Bar <roi3bar@gmail.com>
2024-01-30 17:09:34 +0200

    simplified rotational sensor helper to one-directional Rotation2d support

--- src/main/java/frc/robot/utils/RotationalSensorHelper.java
+++ src/main/java/frc/robot/utils/RotationalSensorHelper.java
@@ -3,75 +3,39 @@ package frc.robot.utils;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class RotationalSensorHelper {
-    private double measuredAngleDegreesCW = 0;
-    private double offsetDegreesCW = 0;
+    private Rotation2d measuredAngle;
+    private Rotation2d offset;
 
-    public RotationalSensorHelper(double initialMeasurementDegreesCW, double initialOffsetDegreesCW) {
-        measuredAngleDegreesCW = initialMeasurementDegreesCW;
-        offsetDegreesCW = initialOffsetDegreesCW;
+    public RotationalSensorHelper(Rotation2d initialMeasuredAngle, Rotation2d initialOffset) {
+        measuredAngle = initialMeasuredAngle;
+        offset = initialOffset;
     }
 
-    public RotationalSensorHelper(double initialMeasurementDegreesCW) {
-        this(initialMeasurementDegreesCW, 0);
+    public RotationalSensorHelper(Rotation2d initalMeasuredAngle) {
+        this(initalMeasuredAngle, new Rotation2d(0));
     }
 
-    public void update(double measuredAngleDegreesCW) {
-        this.measuredAngleDegreesCW = measuredAngleDegreesCW;
+    public void update(Rotation2d measuredAngle) {
+        this.measuredAngle = measuredAngle;
     }
 
-    public double getRawMeasuredAngleDegreesCW() {
-        return measuredAngleDegreesCW;
+    public Rotation2d getMeasuredAngle() {
+        return measuredAngle;
     }
 
-    public double getRawMeasuredAngleDegreesCCW() {
-        return -measuredAngleDegreesCW;
+    public Rotation2d getAngle() {
+        return measuredAngle.minus(offset);
     }
 
-    public Rotation2d getRawMeasuredAngleCW() {
-        return Rotation2d.fromDegrees(measuredAngleDegreesCW);
+    public void resetAngle(Rotation2d newAngle) {
+        offset = measuredAngle.minus(newAngle);
     }
 
-    public Rotation2d getRawMeasuredAngleCCW() {
-        return Rotation2d.fromDegrees(-measuredAngleDegreesCW);
+    public void setOffset(Rotation2d offset) {
+        this.offset = offset;
     }
 
-    public double getAngleDegreesCW() {
-        return measuredAngleDegreesCW - offsetDegreesCW;
-    }
-
-    public double getAngleDegreesCCW() {
-        return -getAngleDegreesCW();
-    }
-
-    public Rotation2d getAngleCW() {
-        return Rotation2d.fromDegrees(getAngleDegreesCW());
-    }
-
-    public Rotation2d getAngleCCW() {
-        return Rotation2d.fromDegrees(getAngleDegreesCCW());
-    }
-
-    public void setOffsetDegreesCW(double offsetDegreesCW) {
-        this.offsetDegreesCW = offsetDegreesCW;
-    }
-
-    public double getOffsetDegreesCW() {
-        return offsetDegreesCW;
-    }
-
-    public void setAngleDegreesCW(double angleDegreesCW) {
-        offsetDegreesCW = measuredAngleDegreesCW - angleDegreesCW;
-    }
-
-    public void setAngleCW(Rotation2d angleCW) {
-        setAngleDegreesCW(angleCW.getDegrees());
-    }
-
-    public void setAngleDegreesCCW(double angleDegreesCCW) {
-        setAngleDegreesCW(-angleDegreesCCW);
-    }
-
-    public void setAngleCCW(Rotation2d angleCCW) {
-        setAngleDegreesCCW(angleCCW.getDegrees());
+    public Rotation2d getOffset() {
+        return offset;
     }
 }
