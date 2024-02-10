834df1ac31718bf192e92af2c2734951c2b4f6f2
Ruth Bilu <ruti.bilu@gmail.com>
2024-02-10 17:41:47 +0200

    added empty commands file

---
+++ src/main/java/frc/robot/subsystems/loader/LoaderCommands.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.loader;
+
+public class LoaderCommands {
+    public final Loader loader;
+
+    public LoaderCommands(Loader loader) {
+        this.loader = loader;
+    }
+}
