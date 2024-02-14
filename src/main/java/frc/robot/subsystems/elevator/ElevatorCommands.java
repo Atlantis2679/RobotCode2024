c99a69e8a33f2a086f8502341a70db2c8984690e
Ruth Bilu <ruti.bilu@gmail.com>
2024-02-14 17:51:55 +0200

    elevator command

--- src/main/java/frc/robot/subsystems/elevator/ElevatorCommands.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorCommands.java
@@ -1,15 +1,38 @@
 
 package frc.robot.subsystems.elevator;
 
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class ElevatorCommands {
-    private final Elevator elevator;
+    private final Elevator elevatorRight;
+    private final Elevator elevatorLeft;
 
-    public ElevatorCommands(Elevator elevator){
-        this.elevator = elevator;
+    public ElevatorCommands(Elevator elevatorLeft, Elevator elevatorRight){
+        this.elevatorRight = elevatorRight;
+        this.elevatorLeft = elevatorLeft;
     }
-    public Command openRight(Double speed){
-        return elevator.setSpeedRight(speed);
+
+    public void manualControl (BooleanSupplier a, BooleanSupplier b, BooleanSupplier c, BooleanSupplier d){
+        if (a.getAsBoolean() == true){
+            elevatorRight.setSpeedRight(ElevatorConstants.SPEED);
+        }
+        if (b.getAsBoolean() == true){
+            elevatorLeft.setSpeedLeft(ElevatorConstants.SPEED);
+        }
+
+        if (c.getAsBoolean() == true){
+            elevatorLeft.setSpeedRight(-ElevatorConstants.SPEED);
+        }
+
+        if (d.getAsBoolean() == true){
+            elevatorLeft.setSpeedLeft(-ElevatorConstants.SPEED);
+        }
+    }
+
+    public Command mCommand(){
+        return manualControl(null, null, null, null).run(() ->);;
     }
 }
